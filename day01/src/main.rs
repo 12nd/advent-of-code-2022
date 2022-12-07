617b1699410daabe00d75593e36db71be0834824
James Ho <ho.james142@gmail.com>
2022-12-07 23:04:45 +1100

    Day 01 pt 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -6,13 +6,17 @@ fn main() {
     
     let inp_data = fs::read_to_string(&args[1]).expect("Error reading input file");
 
-    let calories: Vec<i32> = inp_data
+    let mut calories: Vec<i32> = inp_data
         .split("\n\n")
         .map(|x| x.lines()
              .map(|y| y.parse::<i32>().unwrap())
              .sum())
         .collect::<Vec<i32>>();
 
+    // part 1
     println!("{}", calories.iter().max().unwrap());
+    // part 2
+    calories.sort();
+    println!("{}", calories.iter().rev().take(3).sum::<i32>());
 
 }
