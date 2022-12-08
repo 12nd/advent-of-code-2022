b0bf6a5c4836d237ae700ad84b7e7a14a9232daf
James Ho <ho.james142@gmail.com>
2022-12-08 23:27:43 +1100

    Day 03 pt 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,21 +1,27 @@
 use std::env;
 use std::fs;
 
-fn in_both(a: &str, b: &str) -> usize{
-    for x in a.chars() {
-        for y in b.chars() {
+fn in_both(a: &[u8], b: &[u8]) -> Vec<u8>{
+    let mut out: Vec<u8> = Vec::new();
+    for x in a {
+        for y in b {
             if x == y {
-                // if lowercase
-                if x.is_lowercase(){
-                    return (x as u8 - b'a' + 1) as usize;
-                } else {
-                    return (x as u8 - b'A' + 27) as usize;
-                }
+                out.push(*x);
             }
         }
     }
-    return 0;
+    return out;
 }
+
+fn prio(x: u8) -> usize{
+    // if lowercase
+    if (b'a'..b'z'+1).contains(&x) {
+        return (x - b'a' + 1) as usize;
+    } else {
+        return (x - b'A' + 27) as usize;
+    }
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -23,11 +29,21 @@ fn main() {
     let inp_data = fs::read_to_string(&args[1]).expect("Error reading input file");
 
     let part1: usize = inp_data
+        .trim()
         .lines()
-        .map(|l| in_both(&l[..l.len()/2], &l[l.len()/2..]))
+        .map(|l| prio(in_both(&l[..l.len()/2].as_bytes(), &l[l.len()/2..].as_bytes())[0]))
         .sum();
 
+    let part2: usize = inp_data
+        .trim()
+        .lines()
+        .map(|x| x.as_bytes())
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .map(|x| prio(in_both(x[0], &in_both(x[1],x[2]))[0]))
+        .sum();
 
-    println!("{}", part1)
+    println!("{}", part1);
+    println!("{}", part2);
 
 }
