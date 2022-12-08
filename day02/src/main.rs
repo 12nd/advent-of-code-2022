ab2c1795113a871bcb53ca45037e88bb9b6d01a2
James Ho <ho.james142@gmail.com>
2022-12-08 13:20:02 +1100

    Day 02 pt 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -7,36 +7,62 @@ fn main() {
     let inp_data = fs::read_to_string(&args[1]).expect("Error reading input file");
 
     let mut part1: i32 = 0;
+    let mut part2: i32 = 0;
 
     for game in inp_data.trim().lines() {
         let mut items = game.split(" ");
         let a = items.next().unwrap();
         let b = items.next().unwrap();
 
+        // rock / lose
         if b == "X" {
             part1 += 1;
+            // scissors
             if a == "C" {
                 part1 += 6;
+                part2 += 2; // paper
+            // rock
             } else if a == "A" {
                 part1 += 3;
+                part2 += 3; // scissors
+            } else {
+                part2 += 1; // rock
             }
+
+        // paper / draw
         } else if b == "Y" {
             part1 += 2;
+            part2 += 3;
+            // rock
             if a == "A" {
                 part1 += 6;
+                part2 += 1; // rock
+            // paper
             } else if a == "B" {
                 part1 += 3;
+                part2 += 2; // paper
+            } else {
+                part2 += 3; // scissors
             }
+        
+        // scissors / win
         } else if b == "Z" {
             part1 += 3;
+            part2 += 6;
+            // paper
             if a == "B" {
                 part1 += 6;
+                part2 += 3; // scissors
+            // scissors
             } else if a == "C" {
                 part1 += 3;
+                part2 += 1; // rock
+            } else {
+                part2 += 2; // paper
             }
         }
     }
 
-    println!("{}", part1)
+    println!("{}\n{}", part1, part2);
 
 }
