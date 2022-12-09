3c67a65df0924f0da7f477a8ecc2c8c82cefe0dc
James Ho <ho.james142@gmail.com>
2022-12-09 19:30:06 +1100

    Day 04 pt 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -2,8 +2,13 @@ use std::env;
 use std::fs;
 
 fn pair_contains(x: Vec<usize>) -> bool {
-    // return a >= c && b <= d || c >=a  && d <= b;
+    // return a >= c && b <= d || c >= a  && d <= b;
     return (x[0] >= x[2] && x[1] <= x[3]) || (x[2] >= x[0] && x[3] <= x[1]);
+}
+
+fn pair_overlaps(x: Vec<usize>) -> bool {
+    // return a <= d && b >= c || d <= a  && c >= b;
+    return (x[0] <= x[3] && x[1] >= x[2]) || (x[3] <= x[0] && x[2] >= x[1]);
 }
 
 fn main() {
@@ -14,11 +19,21 @@ fn main() {
     let part1: usize = inp_data
         .trim()
         .lines()
-        .map(|x| x.split([',','-'].as_ref())
-             .map(|y| y.parse::<usize>().unwrap())
-             .collect())
-        .map(|y| pair_contains(y) as usize)
+        .map(|x| x.split([',','-'].as_ref()) // split by both delimiters
+             .map(|y| y.parse::<usize>().unwrap()) // convert string to integer
+             .collect()) // collect into a vector
+        .map(|z| pair_contains(z) as usize)
+        .sum();
+
+    let part2: usize = inp_data
+        .trim()
+        .lines()
+        .map(|x| x.split([',','-'].as_ref()) // split by both delimiters
+             .map(|y| y.parse::<usize>().unwrap()) // convert string to integer
+             .collect()) // collect into a vector
+        .map(|z| pair_overlaps(z) as usize)
         .sum();
 
     println!("{}", part1);
+    println!("{}", part2);
 }
