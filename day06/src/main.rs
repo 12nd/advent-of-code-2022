412d899cbaa3e554baf51500fd177bc7221d2cf7
James Ho <ho.james142@gmail.com>
2022-12-10 19:19:57 +1100

    Day 06 pt 1

---
+++ day06/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::fs;
+use itertools::Itertools;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let inp_data = fs::read_to_string(&args[1]).expect("Error reading input file");
+
+    let part1: usize = inp_data
+        .trim()
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(4)
+        .position(|x| x.iter().all_unique())
+        .map(|x| x + 4)
+        .unwrap();
+
+    println!("{}", part1);
+}
